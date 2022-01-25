8016960fa3fbc1dafe5ec0b11eee31a6a947bcb9
gav974ordi1 <eclapier@msn.com>
2022-01-25 13:21:17 +0400

    creation page client

--- client/client.php
+++ client/client.php
@@ -4,14 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <title>Espace Client</title>
 </head>
 <body>
-<?php
-include '../client/back/dbconnect/connect.php'
-?>
-<pre>
-    <?php echo $stmt; ?> 
-</pre>
+<div class="container-fluid bg-success ">
+    <div class="row">
+        <h1>Espace Client</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-3 ">
+            <p>nom:</p>
+        </div>
+        <div class="col-3"></div>
+        <p>prenom:</p>
+        <div class="col-3"></div>
+        <p>date naisance:</p>
+        <div class="col-3"></div>
+        <p>numero de telephone :</p>
+        <div class="col-3"></div>
+        <p>Adresse:</p>
+        <div class="col-3"></div>
+        Offre:
+        <div class="col-3">
+            date de facturation:
+        </div>
+
+        <div class="col-3"></div>
+        <p>coordonnées bancaires:</p>
+    </div>
+</div>
+
+
 </body>
 </html>
